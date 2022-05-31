cd17e43c071ebe02777edad30b6afa34ff8962dd
Enrique Verea <kikeverea@gmail.com>
2022-05-31 17:24:52 +0200

    First commit

---
+++ src/test/java/com/cac/labs/chucknorris/ChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cac.labs.chucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
