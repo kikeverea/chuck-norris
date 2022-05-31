91c8aa1e316fdf2384b18afef10c8aea19f16ef1
Enrique Verea <kikeverea@gmail.com>
2022-05-31 23:44:56 +0200

    Create JokeController

---
+++ src/main/java/com/cac/labs/chucknorris/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.cac.labs.chucknorris.controllers;
+
+import com.cac.labs.chucknorris.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.generateJoke());
+
+        return "index";
+    }
+}
